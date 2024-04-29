9e1b2e6f6f67549e74d2c5090946e67afab83b96
Melli11 <mingerez@gmail.com>
2024-04-29 17:42:50 -0300

    Limpiando rama master

--- src/main/java/domain/TestTemplateMethod.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-public class TestTemplateMethod {
-    public static void main(String[] args) {
-        TemplateMethod template = new ConcreteTemplate(); // observar como se instancia una clase perteneciente a una clase abstracta.
-        template.TemplateMethod();
-    }
-}
